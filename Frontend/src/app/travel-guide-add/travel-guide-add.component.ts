b27c2424493d0f35c9811b110b9087b7441bbee1
Maneesha <maneeshalakshani9@gmail.com>
2022-07-22 17:00:13 +0530

    TravelGuide-Add component created

---
+++ Frontend/src/app/travel-guide-add/travel-guide-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-travel-guide-add',
+  templateUrl: './travel-guide-add.component.html',
+  styleUrls: ['./travel-guide-add.component.css']
+})
+export class TravelGuideAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
