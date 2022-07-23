ee612c770ffc9f3a12ec497bdc881696a8061123
kavindu <it20009540@my.sliit.lk>
2022-07-23 20:43:18 +0530

    backend commit

---
+++ Backend/src/main/java/com/backend/project/entity/sample.java
@@ -0,0 +1,5 @@
+package com.backend.project.entity;
+
+public class sample {
+
+}
---
+++ Frontend/src/app/one-vehicle/one-vehicle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-one-vehicle',
+  templateUrl: './one-vehicle.component.html',
+  styleUrls: ['./one-vehicle.component.css']
+})
+export class OneVehicleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
