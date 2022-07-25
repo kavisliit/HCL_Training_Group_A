85d257eabd393f6d841360b16f0a7e963e05302d
Tharu119 <tharukamadhushanka9558@gmail.comgit config --global user.name Tharu119git config --global user.email tharukamadhushanka9558@gmail.com>
2022-07-25 11:32:04 +0530

    add_route

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -6,6 +6,10 @@ import { TravelGuideAddComponent } from './travel-guide-add/travel-guide-add.com
 import { TravelGuideRUDComponent } from './travel-guide-rud/travel-guide-rud.component';
 import { UpdateVehicleComponent } from './update-vehicle/update-vehicle.component';
 import { VehicleListComponent } from './vehicle-list/vehicle-list.component';
+import { HotelDetailsComponent } from './hotel_management/hotel-details/hotel-details.component';
+import { HotelListComponent } from './hotel_management/hotel-list/hotel-list.component';
+import { CreateHotelComponent } from './hotel_management/create-hotel/create-hotel.component';
+import { UpdateHotelComponent } from './hotel_management/update-hotel/update-hotel.component';
 
 const routes: Routes = [
   { path: "addTravelGuide", component: TravelGuideAddComponent },
@@ -14,6 +18,12 @@ const routes: Routes = [
   { path: "onevehicle", component: OneVehicleComponent },
   { path: "addvehicle", component: AddVehicleComponent },
   { path: "updatevehicle", component: UpdateVehicleComponent },
+
+  {path: 'hotels', component: HotelListComponent},
+  {path: 'create-hotel', component: CreateHotelComponent},
+  {path: '', redirectTo: 'hotels', pathMatch: 'full'},
+  {path: 'update-hotel/:id', component: UpdateHotelComponent},
+  {path: 'hotel-details/:id', component: HotelDetailsComponent}
 ];
 
 @NgModule({
